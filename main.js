badfc9e41a34f3dece3fce0b472ff135c9de89c4
connielo0112 <connielo0112@gmail.com>
2021-12-01 15:12:51 +0800

    認可

--- main.js
+++ main.js
@@ -9,59 +9,59 @@ const productData = [
   {
     id: "product-1",
     imgUrl:
-      "https://1.bp.blogspot.com/-jUldvEFBoWo/YOwvXfmoIdI/AAAAAAAGEDA/xxhBFKoZANoCXYFOFhaUfrnAiLBwwAhUACPcBGAsYHg/s800/12.jpg",
-    name: "師園滷味",
+      "https://www.ihergo.com/photo/product/11/750_11_1620725353848.jpg",
+    name: "鹹酥雞",
     price: 90
   },
   {
     id: "product-2",
     imgUrl:
-      "https://1.bp.blogspot.com/-jUldvEFBoWo/YOwvXfmoIdI/AAAAAAAGEDA/xxhBFKoZANoCXYFOFhaUfrnAiLBwwAhUACPcBGAsYHg/s800/12.jpg",
-    name: "草莓",
+      "https://i.beauty321.com/816x/https://il.beauty321.com/gallery/gallery/31731/photo-5e8ee702d5cca.jpg",
+    name: "蘿蔔糕",
     price: 60
   },
   {
     id: "product-3",
     imgUrl:
-      "https://1.bp.blogspot.com/-jUldvEFBoWo/YOwvXfmoIdI/AAAAAAAGEDA/xxhBFKoZANoCXYFOFhaUfrnAiLBwwAhUACPcBGAsYHg/s800/12.jpg",
-    name: "奶茶",
-    price: 100
+      "https://d3l76hx23vw40a.cloudfront.net/recipe/gy36-031.jpg",
+    name: "四季豆",
+    price: 60
   },
   {
     id: "product-4",
     imgUrl:
-      "https://1.bp.blogspot.com/-jUldvEFBoWo/YOwvXfmoIdI/AAAAAAAGEDA/xxhBFKoZANoCXYFOFhaUfrnAiLBwwAhUACPcBGAsYHg/s800/12.jpg",
-    name: "冰咖啡",
-    price: 180
+      "https://picdn.gomaji.com/uploads/stores/312/142312/232114/IMG_0309.jpg",
+    name: "雞蛋豆腐",
+    price: 60
   },
   {
     id: "product-5",
     imgUrl:
-      "https://1.bp.blogspot.com/-jUldvEFBoWo/YOwvXfmoIdI/AAAAAAAGEDA/xxhBFKoZANoCXYFOFhaUfrnAiLBwwAhUACPcBGAsYHg/s800/12.jpg",
-    name: "冰拿鐵",
+      "https://img.ltn.com.tw/Upload/food/page/2014/12/02/141202-152-3-gDo0n.jpg",
+    name: "炸湯圓",
     price: 220
   },
   {
     id: "product-6",
     imgUrl:
-      "https://1.bp.blogspot.com/-jUldvEFBoWo/YOwvXfmoIdI/AAAAAAAGEDA/xxhBFKoZANoCXYFOFhaUfrnAiLBwwAhUACPcBGAsYHg/s800/12.jpg",
-    name: "冰可可",
+      "https://d31pvszaz82ppm.cloudfront.net/82909853-30b0-43c2-bce9-6e48b6f440d4_480.jpg",
+    name: "炸魷魚",
     price: 220
   },
   {
     id: "product-7",
     imgUrl:
-      "https://1.bp.blogspot.com/-jUldvEFBoWo/YOwvXfmoIdI/AAAAAAAGEDA/xxhBFKoZANoCXYFOFhaUfrnAiLBwwAhUACPcBGAsYHg/s800/12.jpg",
-    name: "冰可樂",
-    price: 120
+      "https://nash.tw/wp-content/uploads/DSC_4327_%E7%BB%93%E6%9E%9C-1-scaled.jpg",
+    name: "綜合套餐 B",
+    price: 170
   },
   {
     id: "product-8",
     imgUrl:
-      "https://1.bp.blogspot.com/-jUldvEFBoWo/YOwvXfmoIdI/AAAAAAAGEDA/xxhBFKoZANoCXYFOFhaUfrnAiLBwwAhUACPcBGAsYHg/s800/12.jpg",
-    name: "咖喱飯",
-    price: 520
-  }
+      "https://im.marieclaire.com.tw/m800c533h100b0/assets/mc/202004/5E8F0A1C395F91586432540.jpeg",
+    name: "豪華套餐 A",
+    price: 260
+  },
 ];
 // ======= 請從這裡開始 =======
 //1.將菜單資料放到menu
@@ -75,7 +75,7 @@ function displayＭenu(products) {
         <div class="card">
           <img src="${product.imgUrl}" class="card-img-top" alt="...">
           <div class="card-body">
-            <h5 class="card-title">${product.name}</h5>
+            <h4 class="card-title">${product.name}</h4>
             <p class="card-text">${product.price}</p>
             <button class="btn btn-primary order-btn" id="${product.id}">加入購物車</button>
           </div>
